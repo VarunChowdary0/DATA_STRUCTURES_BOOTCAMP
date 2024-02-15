b820f1c9b34dfea98753af12505f6bc9f8c0981e
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-02-15 21:16:13 +0530

    ,

--- Graphs/DFS.py
+++ Graphs/DFS.py
@@ -1,14 +1,10 @@
-def dfs(graph,visited,start):
-    
+def dfs(graph,visited,start): 
     nodes =  ['A','B','C','D','E','F']
-
     visited[start] = True
     print(nodes[start],end=" ")
-
     for i in range(len(graph)):
         if graph[start][i]==1 and not visited[i]:
             dfs(graph,visited,i)
-
 graph = [
     [ 1, 0, 1, 0, 0, 1],
     [ 0, 0, 0, 1, 1, 1],
@@ -17,7 +13,6 @@ graph = [
     [ 1, 1, 1, 1, 0, 0],
     [ 0, 1, 0, 1, 0, 1]
 ]
-
 visited = [False] * len(graph)
 
 dfs(graph,visited,0)
--- LinkedList.py/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-DL.Insert_End(200)
-DL.Insert_End(300)
-DL.Insert_End(400)
